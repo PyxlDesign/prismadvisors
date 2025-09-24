f445b6e2c87610ba4a79e641189dea983c1b1eeb
Chris Schultz <Chris@pyxldesign.com>
2025-09-23 23:50:36 -0500

    Remove lib

--- lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-// lib/supabase.ts
-import { createClient } from "@supabase/supabase-js";
-
-const url = process.env.SUPABASE_URL!;
-const service = process.env.SUPABASE_SERVICE_ROLE!; // server-side only
-
-export const supabaseAdmin = createClient(url, service, {
-    auth: { persistSession: false },
-});
